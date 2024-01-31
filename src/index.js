65b5dcee6fcfa3135921535d7dd9e12c0159571c
achintha <achinthajayalal001@gmail.com>
2024-01-31 20:13:28 +0530

    updated index.html, App.js & index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const container1 = document.getElementById('header1');
+const container2 = document.getElementById('header2');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root1 = ReactDOM.createRoot(container1);
+const root2 = ReactDOM.createRoot(container2);
+
+root1.render(<h1>Test app_1</h1>);
+root2.render(<h1>Test app_2</h1>);
